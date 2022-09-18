0b93392f48dc6885de2ae6cabbd4b6e147268893
Helasdic <helasdicakra@gmail.com>
2022-09-19 03:39:47 +0700

    updateaja

--- resources/views/pages/whychooseus.blade.php
+++ resources/views/pages/whychooseus.blade.php
@@ -1,13 +1,11 @@
 <div class="col-md-6"></div>
 <div class="col-md-6">
     <div class="heading-section heading-section-1 dark">
-        <h3>Why Choose Us</h3>
+        <h3>Mengapa harus kami?</h3>
     </div>
-    <p class="text-block m-b-25">Who we are, how we work, our priorities and our values. Phasellus
-        mauris arcu, facilisis vel velit vitae, tincidunt scelerisque mauris. Phasellus placerat
-        elementum ornare. Cras scelerisque eu metus tincidunt euismod.</p>
+    <p class="text-block m-b-25">Ini adalah alasan mengapa klien memilih kami. Mempercayakan kami untuk </p>
     <div class="progress-box progress-box-1 m-b-25">
-        <p class="progress-label">INCOME</p>
+        <p class="progress-label">PEMASUKAN</p>
         <div class="au-progress au-progress-1">
             <div class="au-progress-bar" role="progressbar" data-transitiongoal="39">
                 <span></span>
@@ -15,15 +13,15 @@
         </div>
     </div>
     <div class="progress-box progress-box-1 m-b-25">
-        <p class="progress-label">OPPORTUNITY</p>
+        <p class="progress-label">KEUNTUNGAN</p>
         <div class="au-progress au-progress-1">
-            <div class="au-progress-bar" role="progressbar" data-transitiongoal="79">
+            <div class="au-progress-bar fw-bold" role="progressbar" data-transitiongoal="79">
                 <span></span>
             </div>
         </div>
     </div>
     <div class="progress-box progress-box-1">
-        <p class="progress-label">TRANSFORMATION</p>
+        <p class="progress-label">TRANSFORMSI</p>
         <div class="au-progress au-progress-1">
             <div class="au-progress-bar" role="progressbar" data-transitiongoal="55">
                 <span></span>
