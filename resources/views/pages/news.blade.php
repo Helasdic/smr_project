a21db62b90dffb7a703923b5b0f18518e8f32ac4
oksa satya <78420053+oksasatya@users.noreply.github.com>
2022-09-20 14:03:54 +0700

    fitur news diganti jadi our service

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -1,31 +1,33 @@
 <div class="heading-section heading-section-1 dark">
-    <h3>Latest News</h3>
+    <h3>Mengapa Harus Kami</h3>
 </div>
 <div class="owl-carousel dark nav-style-1" data-carousel-margin="45" data-carousel-nav="true" data-carousel-loop="true">
     <div class="image-card image-card-4">
         <div class="image">
-            <a href="{{ url('/single-blog') }}">
-                <img src="https://picsum.photos/1600/870?random={{ rand(1, 100) }}" alt="image">
+            <a href="#">
+                {{-- generate random unsplasj 1600x870 --}}
+                <img src="https://source.unsplash.com/random/1600x870/?office={{ rand(1, 100) }}"
+                    alt="Business growth solutions" />
             </a>
         </div>
         <div class="date">
             <span>July 22, 2018</span>
         </div>
         <h3 class="title">
-            <a href="{{ url('/single-blog') }}">Improve margins and reposition quickly</a>
+            <a href="#">Memberikan jasa pembuatan laporan pajak bulanan, dan tahunan</a>
         </h3>
         <div class="content">
             <p>Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula
                 eget tortor tempor, quis porta tellus dictum.</p>
         </div>
         <div class="link">
-            <a href="{{ url('/single-blog') }}">Continue reading</a>
+            <a href="#">Continue reading</a>
         </div>
     </div>
     <div class="image-card image-card-4">
         <div class="image">
-            <a href="{{ url('/single-blog') }}">
-                <img src="https://picsum.photos/1600/870?random={{ rand(1, 100) }}" alt="image">
+            <a href="#">
+                <img src="https://source.unsplash.com/random/1600x870/?office={{ rand(1, 100) }}" alt="image">
 
             </a>
         </div>
@@ -33,60 +35,61 @@
             <span>July 18, 2018</span>
         </div>
         <h3 class="title">
-            <a href="{{ url('/single-blog') }}">Delight customers and operate faster</a>
+            <a href="#">Membantu menyelesaikan laporan pajak terkait pembayaran </a>
         </h3>
         <div class="content">
             <p>Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula
                 eget tortor tempor, quis porta tellus dictum.</p>
         </div>
         <div class="link">
-            <a href="{{ url('/single-blog') }}">Continue reading</a>
+            <a href="#">Continue reading</a>
         </div>
     </div>
     <div class="image-card image-card-4">
         <div class="image">
-            <a href="{{ url('/single-blog') }}">
-                <img src="https://picsum.photos/1600/870?random={{ rand(1, 100) }}" alt="image">
+            <a href="#">
+                <img src="https://source.unsplash.com/random/1600x870/?office={{ rand(1, 100) }}" alt="image">
             </a>
         </div>
         <div class="date">
             <span>July 12, 2018</span>
         </div>
         <h3 class="title">
-            <a href="{{ url('/single-blog') }}">Strategy experience and analytical</a>
+            <a href="#">Jasa penyelesaian laporan pajak melalui media yang disediakan oleh Dirjen Pajak Indonesia
+            </a>
         </h3>
         <div class="content">
             <p>Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula
                 eget tortor tempor, quis porta tellus dictum.</p>
         </div>
         <div class="link">
-            <a href="{{ url('/single-blog') }}">Continue reading</a>
+            <a href="#">Continue reading</a>
         </div>
     </div>
     <div class="image-card image-card-4">
         <div class="image">
-            <a href="{{ url('/single-blog') }}">
-                <img src="https://picsum.photos/1600/870?random={{ rand(1, 100) }}" alt="image">
+            <a href="#">
+                <img src="https://source.unsplash.com/random/1600x870/?office={{ rand(1, 100) }}" alt="image">
             </a>
         </div>
         <div class="date">
             <span>July 22, 2018</span>
         </div>
         <h3 class="title">
-            <a href="{{ url('/single-blog') }}">Improve margins and reposition quickly</a>
+            <a href="#">Jasa Penyelesaian dan atau maintenance terkait permasalahan perpajakan perusahaan</a>
         </h3>
         <div class="content">
             <p>Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula
                 eget tortor tempor, quis porta tellus dictum.</p>
         </div>
         <div class="link">
-            <a href="{{ url('/single-blog') }}">Continue reading</a>
+            <a href="#">Continue reading</a>
         </div>
     </div>
     <div class="image-card image-card-4">
         <div class="image">
-            <a href="{{ url('/single-blog') }}">
-                <img src="https://picsum.photos/1600/870?random={{ rand(1, 100) }}" alt="image">
+            <a href="#">
+                <img src="https://source.unsplash.com/random/1600x870/?office={{ rand(1, 100) }}" alt="image">
 
             </a>
         </div>
@@ -94,34 +97,14 @@
             <span>July 18, 2018</span>
         </div>
         <h3 class="title">
-            <a href="{{ url('/single-blog') }}">Delight customers and operate faster</a>
+            <a href="#">Jasa Konsultasi perpajakan Lainnya </a>
         </h3>
         <div class="content">
             <p>Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula
                 eget tortor tempor, quis porta tellus dictum.</p>
         </div>
         <div class="link">
-            <a href="{{ url('/single-blog') }}">Continue reading</a>
-        </div>
-    </div>
-    <div class="image-card image-card-4">
-        <div class="image">
-            <a href="{{ url('/single-blog') }}">
-                <img src="https://picsum.photos/1600/870?random={{ rand(1, 100) }}" alt="image">
-            </a>
-        </div>
-        <div class="date">
-            <span>July 12, 2018</span>
-        </div>
-        <h3 class="title">
-            <a href="{{ url('/single-blog') }}">Strategy experience and analytical</a>
-        </h3>
-        <div class="content">
-            <p>Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula
-                eget tortor tempor, quis porta tellus dictum.</p>
-        </div>
-        <div class="link">
-            <a href="{{ url('/single-blog') }}">Continue reading</a>
+            <a href="#">Continue reading</a>
         </div>
     </div>
 </div>
